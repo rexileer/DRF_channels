29ed7a33b068ccdc1563d7634a7f64ef4b11bf4b
rexileer <110720074+rexileer@users.noreply.github.com>
2024-12-12 17:50:03 +0300

    test drf

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'example', views.ExampleViewSet, basename='example')
 
 urlpatterns = [
-    path('test/', views.test_view, name='test_view')
+    path('test/', views.test_view, name='test_view'),
+    path('', include(router.urls)),
+    path('simple-endpoint/', views.simple_view, name='simple_endpoint')
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -1,6 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.viewsets import ViewSet
+
+@api_view(['GET'])
+def simple_view(request):
+    return Response({'message': 'Simple endpoint'})
 
 @api_view(['GET'])
 def test_view(request):
     return Response({'message': 'DRF is working!'})
+
+
+    
+
+class ExampleViewSet(ViewSet):
+    def list(self, request):
+        return Response({'message': 'List of examples'})
+    
+    def create(self, request):
+        return Response({'message': 'Example created'})
+    
+    def retrieve(self, request, pk=None):
+        return Response({'message': f'Example {pk}'})
+    
