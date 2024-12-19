f89eb699ed064f25177d1811e95b165b9fcd7c42
rexileer <110720074+rexileer@users.noreply.github.com>
2024-12-19 12:32:57 +0300

    create chat.html

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     return render(request, 'core/index.html')
+
+
+def chat(request):
+    return render(request, 'core/chat.html')
