4b97157450a1bb0f1d7c58669dc0bc1f8543cae9
rexileer <110720074+rexileer@users.noreply.github.com>
2024-12-20 19:28:30 +0300

    Update serializers.py
    ChatHistorySerializer

--- myproject/api/serializers.py
+++ myproject/api/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
+from core.models import ChatMessage
+
 
 class ChatMessageSerializer(serializers.Serializer):
     message = serializers.CharField(max_length=500)  # Входящее сообщение
     response = serializers.CharField(max_length=500, read_only=True)  # Ответ (только для чтения)
+
+
+class ChatHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatMessage
+        fields = ['user_message', 'llm_response', 'created_at']
