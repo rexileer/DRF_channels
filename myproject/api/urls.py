2d4f1f0dc4a0d083e2f358b68077be7db9120ab6
rexileer <110720074+rexileer@users.noreply.github.com>
2024-12-20 19:12:24 +0300

    Update urls.py
    ChatAPIView

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -8,5 +8,6 @@ router.register(r'example', views.ExampleViewSet, basename='example')
 urlpatterns = [
     path('test/', views.test_view, name='test_view'),
     path('', include(router.urls)),
-    path('simple-endpoint/', views.simple_view, name='simple_endpoint')
+    path('simple-endpoint/', views.simple_view, name='simple_endpoint'),
+    path('chat/', views.ChatAPIView.as_view(), name='chat_api'),
 ]
