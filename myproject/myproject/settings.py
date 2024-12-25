c61ba60b86d79a79e40a24c181661192eb3c7a53
rexileer <110720074+rexileer@users.noreply.github.com>
2024-12-25 13:27:22 +0300

    Update settings.py
    .env + import load_env
    GROQ_API_KEY

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +137,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
